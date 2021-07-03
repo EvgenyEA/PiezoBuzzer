a42b7e32c058e2f0f80262a608964302f1187dde
EvgenyEA <44459511+EvgenyEA@users.noreply.github.com>
2021-07-04 01:20:47 +0400

    Add files via upload

---
+++ notes.h
@@ -0,0 +1,181 @@
+#ifndef NOTES_H
+#define NOTES_H
+
+#include <stdint.h>
+// notes
+const uint8_t N_c 	= 0;
+const uint8_t N_his = 0;
+const uint8_t N_cis = 1;
+const uint8_t N_des = 1;
+const uint8_t N_d 	= 2;
+const uint8_t N_dis = 3;
+const uint8_t N_es 	= 3;
+const uint8_t N_e 	= 4;
+const uint8_t N_fes = 4;
+const uint8_t N_f 	= 5;
+const uint8_t N_eis = 5;
+const uint8_t N_fis = 6;
+const uint8_t N_ges = 6;
+const uint8_t N_g 	= 7;
+const uint8_t N_gis = 8;
+const uint8_t N_as 	= 8;
+const uint8_t N_a 	= 9;
+const uint8_t N_ais = 10;
+const uint8_t N_b 	= 10;
+const uint8_t N_h 	= 11;
+const uint8_t N_ces = 11;
+
+const uint8_t N_c1 		= N_c 	+ 12;
+const uint8_t N_his1 	= N_his + 12;
+const uint8_t N_cis1 	= N_cis + 12;
+const uint8_t N_des1 	= N_cis + 12;
+const uint8_t N_d1 		= N_d 	+ 12;
+const uint8_t N_dis1 	= N_dis + 12;
+const uint8_t N_es1 	= N_es 	+ 12;
+const uint8_t N_e1 		= N_e 	+ 12;
+const uint8_t N_fes1 	= N_fes + 12;
+const uint8_t N_f1 		= N_f 	+ 12;
+const uint8_t N_eis1 	= N_eis + 12;
+const uint8_t N_fis1 	= N_fis + 12;
+const uint8_t N_ges1 	= N_ges + 12;
+const uint8_t N_g1 		= N_g 	+ 12;
+const uint8_t N_gis1 	= N_gis + 12;
+const uint8_t N_as1 	= N_as 	+ 12;
+const uint8_t N_a1 		= N_a 	+ 12;
+const uint8_t N_ais1 	= N_ais + 12;
+const uint8_t N_b1 		= N_b 	+ 12;
+const uint8_t N_h1 		= N_h 	+ 12;
+const uint8_t N_ces1 	= N_ces + 12;
+
+const uint8_t N_c2 		= N_c 	+ 24;
+const uint8_t N_his2 	= N_his + 24;
+const uint8_t N_cis2 	= N_cis + 24;
+const uint8_t N_des2 	= N_cis + 24;
+const uint8_t N_d2 		= N_d 	+ 24;
+const uint8_t N_dis2 	= N_dis + 24;
+const uint8_t N_es2 	= N_es 	+ 24;
+const uint8_t N_e2 		= N_e 	+ 24;
+const uint8_t N_fes2 	= N_fes + 24;
+const uint8_t N_f2 		= N_f 	+ 24;
+const uint8_t N_eis2 	= N_eis + 24;
+const uint8_t N_fis2 	= N_fis + 24;
+const uint8_t N_ges2 	= N_ges + 24;
+const uint8_t N_g2 		= N_g 	+ 24;
+const uint8_t N_gis2 	= N_gis + 24;
+const uint8_t N_as2 	= N_as 	+ 24;
+const uint8_t N_a2 		= N_a 	+ 24;
+const uint8_t N_ais2 	= N_ais + 24;
+const uint8_t N_b2 		= N_b 	+ 24;
+const uint8_t N_h2 		= N_h 	+ 24;
+const uint8_t N_ces2 	= N_ces + 24;
+
+const uint8_t N_c3 		= N_c 	+ 36;
+const uint8_t N_his3 	= N_his + 36;
+const uint8_t N_cis3 	= N_cis + 36;
+const uint8_t N_des3 	= N_cis + 36;
+const uint8_t N_d3 		= N_d 	+ 36;
+const uint8_t N_dis3 	= N_dis + 36;
+const uint8_t N_es3 	= N_es 	+ 36;
+const uint8_t N_e3 		= N_e 	+ 36;
+const uint8_t N_fes3 	= N_fes + 36;
+const uint8_t N_f3 		= N_f 	+ 36;
+const uint8_t N_eis3 	= N_eis + 36;
+const uint8_t N_fis3 	= N_fis + 36;
+const uint8_t N_ges3 	= N_ges + 36;
+const uint8_t N_g3 		= N_g 	+ 36;
+const uint8_t N_gis3 	= N_gis + 36;
+const uint8_t N_as3 	= N_as 	+ 36;
+const uint8_t N_a3 		= N_a 	+ 36;
+const uint8_t N_ais3 	= N_ais + 36;
+const uint8_t N_b3 		= N_b 	+ 36;
+const uint8_t N_h3 		= N_h 	+ 36;
+const uint8_t N_ces3 	= N_ces + 36;
+
+// tempes
+const uint8_t T_1_16	= 1;
+const uint8_t T_1_8		= 2;
+const uint8_t T_1_4		= 4;
+const uint8_t T_1_2		= 8;
+const uint8_t T_1		= 16;
+
+const uint8_t P_1_16 	= 17;
+const uint8_t P_1_8 	= 18;
+const uint8_t P_1_4		= 20;
+const uint8_t P_1_2		= 28;
+const uint8_t P_1		= 32;
+
+// Winter
+const uint8_t MUSIC_WINTER[] = {
+	N_e2, 	N_e2, 	N_e2, 	N_e2, 	N_e2, 	N_e2, 	N_f2, 	N_f2, 	N_f2, 	N_f2, 	N_f2,
+	N_d2, 	N_d2, 	N_d2, 	N_d2, 	N_d2, 	N_d2, 	N_e2, 	N_e2, 	N_e2, 	N_e2, 	N_e2,
+	N_c2, 	N_c2, 	N_c2, 	N_c2, 	N_c2, 	N_c2, 	N_d2, 	N_d2, 	N_d2, 	N_d2,
+	N_e2, 	N_e2, 	N_e2, 	N_d2, 	N_d2, 	N_d2, 	N_c2, 	N_h1, 	N_a1, 	N_a1
+};
+
+const uint8_t DURATION_WINTER[] = {
+	T_1_8, 	T_1_8, 	T_1_4, 	T_1_8, 	T_1_8, 	T_1_4, 	T_1_4, 	T_1_4, 	T_1_4, 	T_1_8, 	T_1_8, 
+	T_1_8, 	T_1_8, 	T_1_4, 	T_1_8, 	T_1_8, 	T_1_4, 	T_1_4, 	T_1_4, 	T_1_4, 	T_1_8, 	T_1_8,
+	T_1_8, 	T_1_8, 	T_1_4, 	T_1_8, 	T_1_8, 	T_1_4,	T_1_4, 	T_1_4,	T_1_4,	T_1_4,
+	T_1_8, 	T_1_8, 	T_1_4, 	T_1_8, 	T_1_8, 	T_1_4,	T_1_4, 	T_1_4,	T_1_4,	T_1_4
+};
+
+const uint8_t TEMPO_WINTER = 120;
+const uint8_t SIZE_WINTER = sizeof(MUSIC_WINTER);
+
+// Pack of cigaretes
+const uint8_t MUSIC_PACK_OF_CIGARETTES[] = {
+	N_h, 	N_c1, N_h1,	N_a1, N_e1,	N_c1, P_1_8, N_g1,	N_e1,	N_c1, N_d1, N_a1, N_fis1, N_c1, N_h, P_1_8, N_h1,
+	N_a1, N_e1, N_c1, N_g1, N_e1, N_h, N_c1,	P_1_8, N_a1, N_fis1, N_c1, N_h, N_c1, N_e1, N_h1, N_a1, N_e1, N_c1, P_1_8, N_g1, N_e1, N_c1,
+	N_d1, N_a1, N_fis1, N_c1, N_h, P_1_8, N_h1, N_a1, N_e1, N_c1, P_1_8, N_g2, P_1_8, N_d2
+};
+
+const uint8_t DURATION_PACK_OF_CIGARETTES[] = {
+	T_1_8, T_1_8,	T_1_8,	T_1_8,	T_1_8,	T_1_8, P_1_8, T_1_8, T_1_8, T_1_8, T_1_4,	T_1_8,	T_1_8,	T_1_8,	T_1_8, P_1_8, T_1_8,
+	T_1_8, T_1_8, T_1_4,	T_1_8,	T_1_8,	T_1_8,	T_1_8, P_1_8,	T_1_8, T_1_8,	T_1_8,	P_1_4, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, P_1_8, T_1_8, T_1_8, T_1_8,
+	T_1_4,	T_1_8, T_1_8, T_1_8,	T_1_8, P_1_8, T_1_8, T_1_8, T_1_8, T_1_4, P_1_8, T_1_8, P_1_8, T_1_8
+};
+
+const uint8_t TEMPO_PACK_OF_CIGARETTES = 100;
+const uint8_t SIZE_PACK_OF_CIGARETTES = sizeof(MUSIC_PACK_OF_CIGARETTES);
+
+// Careless angel
+const uint8_t MUSIC_CARELESS_ANGEL[] = {
+	N_g1, 	N_a1, 	N_h1, 	N_d2, 	N_fis2, N_e2, 	N_d2, 	P_1_4, 	P_1_16,
+	N_h1, 	N_d2, 	N_c2, 	N_c2, 	N_g1, 	N_h1, 	P_1_16, P_1_2,
+	P_1_4,	N_g1, 	N_a1, 	N_h1, 	N_d2, 	N_d2, 	N_d2, 	N_fis2, N_e2, 	N_e2, 	P_1_4, 	P_1_16,
+	N_d2, 	N_c2, 	N_c2, 	N_h1, 	N_a1, 	N_a1, 	N_a1
+};
+
+const uint8_t DURATION_CARELESS_ANGEL[] = {
+	T_1_8, 	T_1_8, 	T_1_8, 	T_1_8,	T_1_8,	T_1_8,	T_1_4,	P_1_4, 	P_1_8,
+	T_1_8,	T_1_4,	T_1_4,	T_1_4,	T_1_8,	T_1_2,	P_1_16,	P_1_2,
+	P_1_4,	T_1_8,	T_1_8,	T_1_8,	T_1_8,	T_1_8,	T_1_8,	T_1_4,	T_1_8,	T_1_4,	P_1_4,	P_1_16,
+	T_1_4,	T_1_8,	T_1_8 + T_1_4,	T_1_4,	T_1_4,	T_1_8,	T_1_4
+};
+
+const uint8_t TEMPO_CARELESS_ANGEL = 120;
+const uint8_t SIZE_CARELESS_ANGEL = sizeof(MUSIC_CARELESS_ANGEL);
+
+// Wind of change
+const uint8_t MUSIC_WIND_OF_CHANGE[] = {
+	P_1_4, N_a1,	N_h1,	N_c2,	N_g1,	N_f1, N_d1,
+	P_1_4, N_a1,	N_h1,	N_c2, N_d2, N_c2,
+	P_1_4, N_a1, N_h1, N_c2, N_g1, N_f1,
+	N_d1,  N_a1,	N_h1,	N_c2,	N_d2,	N_c2,
+	N_c2,	 N_c2,	N_d2,	N_e2,	N_f2,	N_e2,	N_c2,	N_d2,
+	P_1_2, N_a1,	N_c2,	P_1_4, N_h1
+};
+
+const uint8_t DURATION_WIND_OF_CHANGE[] = {
+	P_1_4, T_1_8, T_1_16, T_1_16 + T_1_4, T_1_16, T_1_16, T_1_8,
+	P_1_4, T_1_8,	T_1_8,	T_1_8,	T_1_16,	T_1_16 + T_1_4,
+	P_1_4, T_1_8,	T_1_16,	T_1_16 + T_1_4, T_1_8, T_1_8,
+	P_1_4, T_1_8,	T_1_8,	T_1_8, T_1_16,	T_1_16 + T_1_4,
+	T_1_4, T_1_8,	T_1_8,	T_1_8, T_1_16,	T_1_16 + T_1_8, T_1_16, T_1_16 + T_1_8,
+	P_1_4, T_1_8, T_1_4, 	P_1_4,	T_1_4
+};
+
+const uint8_t TEMPO_WIND_OF_CHANGE = 76;
+const uint8_t SIZE_WIND_OF_CHANGE = sizeof(MUSIC_WIND_OF_CHANGE);
+
+#endif
